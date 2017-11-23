2ae1b4330bae16e3f249008b821cfefb77d05cc9
Gorilla-Zhang <841845663@qq.com>
2017-11-23 18:38:50 +0800

    second

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -20,8 +20,11 @@ export default class CommentInput extends React.Component {
   }
   handleSubmit() {
     if (this.props.onSubmit) {
-      const { username, content } = this.state
-      this.props.onSubmit({ username, content })
+      this.props.onSubmit({ 
+        username:this.state.username,
+        content: this.state.content,
+        createdTime:+new Date()
+       })
     }
     this.setState({ content: '' })
   }
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import Comment from './Comment'
+import PropTypes from 'prop-types'
 
 export default class CommentInput extends React.Component {
+  static PropTypes = {
+    comments: PropTypes.array,
+    onDeleteComment:PropTypes.func
+  }
   static defaultProps = {
     comments: []
   }
+
+  handleDeleteComment(index){
+    if(this.props.onDeleteComment){
+      this.props.onDeleteComment(index)
+    }
+  }
+
   render() {
     return (
       <div>
         {this.props.comments.map((comment, i) =>
-          <Comment comment={comment} key={i} />
+          <Comment comment={comment} key={i} index={i} onDeleteComment={(e)=>this.handleDeleteComment(e)}/>
         )}
       </div>
     )
